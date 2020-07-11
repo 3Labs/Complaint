54d059dbbb4c45eb0af4b7a92e1a179c32ee23c5
3Labs <tri3labs@gmail.com>
2020-07-11 18:38:06 +0700

    Forge API
    Call a Confluence API

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
-import ForgeUI, { render, Fragment, Macro, Text } from "@forge/ui";
+import api from "@forge/api";
+import ForgeUI, { render, Fragment, Text, Macro, useProductContext, useState } from '@forge/ui';
 
+const fetchCommentsForContent = async (contentId) => {
+  const res = await api
+    .asApp()
+    .requestConfluence(`/wiki/rest/api/content/${contentId}/child/comment`);
+
+  const data = await res.json();
+  return data.results;
+};
 
 const App = () => {
+  const context = useProductContext();
+  const [comments] = useState(async () => await fetchCommentsForContent(context.contentId));
+
+  console.log(`Number of comments on this page: ${comments.length}`);
+
   return (
     <Fragment>
-      <Text>Complaint Handling: Aplikasi Pengaduan Online</Text>
+      <Text>Hello world!</Text>
     </Fragment>
   );
 };
